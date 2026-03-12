dffb28398aec50943e8bcc02a3bd0d5186c81c30
kobeprior99 <129470871+kobeprior99@users.noreply.github.com>
2026-03-12 09:53:20 -0600

    accelerometer good progress

--- Arduino_Files/Smart_Pick/accel/Accelerometer.hpp
+++ Arduino_Files/Smart_Pick/accel/Accelerometer.hpp
@@ -1,8 +1,33 @@
-#ifndef CDC_HPP
-#define CDC_HPP 
+#ifndef ACC_HPP
+#define ACC_HPP
 
 #include <Arduino.h>
 #include <Wire.h>
+
+// ## Accelerometer (I2C Interface)
+// | Signal      | GPIO Pin |
+// |-------------|----------|
+// | SDA         | IO41     |
+// | SCL         | IO40     |
+// | Interrupt 1 | IO36     |
+// | Interrupt 2 | IO35     |
+
+// I2C address
+#define ADXL375_ADDR 0x53
+
+// Registers
+#define REG_POWER_CTL   0x2D
+#define REG_DATA_FORMAT 0x31
+#define REG_BW_RATE     0x2C
+#define REG_INT_ENABLE  0x2E
+#define REG_INT_MAP     0x2F
+#define REG_INT_SOURCE  0x30
+#define REG_DATAX0      0x32
+#define REG_SHX_THRSH   0x1D
+#define REG_SHX_DUR     0x21
+
+// Pins
+#define ACC_INT1_PIN 36
 
 struct AccelData {
     float x;
@@ -12,16 +37,89 @@ struct AccelData {
 
 class ACC {
 public:
-  (TwoWire &wire) : _wire(wire){}
-  void Setup(){
-    //setup code here
-  }
-  bool Interrupt(){
-    //wait for interrupt pin to go high
-  }
+
+    ACC(TwoWire &wire) : _wire(wire) {}
+
+    void Setup() {
+      pinMode(ACC_INT1_PIN, INPUT);
+      // Data rate
+      writeRegister(REG_BW_RATE, 0x0D);   // 800 Hz
+      // Data format
+      writeRegister(REG_DATA_FORMAT, 0x0B);   // full resolution
+      // Shock threshold
+      writeRegister(REG_SHX_THRSH, 40);     // adjust sensitivity 2.5 g
+      // Shock duration
+      writeRegister(REG_SHX_DUR, 20);
+      // Route to INT1 a setting of 0 causes interrupts to be sent to interrupt pin 1
+      writeRegister(0x2F, 0x00);
+      // Enable single shock interrupt
+      writeRegister(REG_INT_ENABLE, 0b01000000);
+      // Enable measurement mode
+      writeRegister(0x2D, 0x08);
+    }
+
+    bool Interrupt1() {
+
+        if (digitalRead(ACC_INT1_PIN)) {
+
+            // clear interrupt by reading source register
+            readRegister(REG_INT_SOURCE);
+
+            return true;
+        }
+
+        return false;
+    }
+
+    AccelData readAccel() {
+
+        uint8_t buffer[6];
+
+        _wire.beginTransmission(ADXL375_ADDR);
+        _wire.write(REG_DATAX0);
+        _wire.endTransmission(false);
+
+        _wire.requestFrom(ADXL375_ADDR, 6);
+
+        for (int i = 0; i < 6; i++) {
+            buffer[i] = _wire.read();
+        }
+
+        int16_t x = buffer[1] << 8 | buffer[0];
+        int16_t y = buffer[3] << 8 | buffer[2];
+        int16_t z = buffer[5] << 8 | buffer[4];
+
+        AccelData data;
+
+        // ~49 mg/LSB for ADXL375
+        data.x = x * 0.049;
+        data.y = y * 0.049;
+        data.z = z * 0.049;
+
+        return data;
+    }
+
 private:
-    AccelData currentData;
-    void readRaw();
+
+    TwoWire &_wire;
+
+    void writeRegister(uint8_t reg, uint8_t value) {
+
+        _wire.beginTransmission(ADXL375_ADDR);
+        _wire.write(reg);
+        _wire.write(value);
+        _wire.endTransmission();
+    }
+
+    uint8_t readRegister(uint8_t reg) {
+
+        _wire.beginTransmission(ADXL375_ADDR);
+        _wire.write(reg);
+        _wire.endTransmission(false);
+
+        _wire.requestFrom(ADXL375_ADDR, (uint8_t)1);
+        return _wire.read();
+    }
 };
 
 #endif
